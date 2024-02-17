ecb6cebe56b1f4ed995ed9e0f08ab147b5aad000
Doryan Dahon <ddahon@gmail.com>
2024-02-18 00:36:00 +0100

    get algolia params from js

--- cmd/scraper/collectors/datadog/datadog.go
+++ cmd/scraper/collectors/datadog/datadog.go
@@ -1,109 +1,76 @@
 package datadog
 
 import (
-	"context"
 	"errors"
+	"fmt"
+	"io"
 	"log"
-	"time"
+	"net/http"
+	"regexp"
 
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/chromedp"
 	"github.com/ddahon/jobboard/internal/models"
-	"github.com/gocolly/colly/v2"
 )
 
 var companyShortname = "datadog"
 var baseDomain = "careers.datadoghq.com"
 var baseUrl = "https://" + baseDomain
+var startingUrl = baseUrl + "/remote"
 var company *models.Company
 
 func Scrape() ([]models.Job, error) {
+	var jobs []models.Job
 	company = models.GetCompanyByShortname(companyShortname)
 	if company == nil {
 		return nil, errors.New("Cannot retrieve company for shortname " + companyShortname + ". Aborting scraping for this company.")
 	}
-	ctx, cancel := chromedp.NewContext(context.Background())
 
-	defer cancel()
-
-	jobLinks, err := getJobLinks(ctx)
+	// get algolia tokens from js
+	algoliaApiKey, algoliaAppId, algoliaIndex, err := getAlgoliaParams()
 	if err != nil {
 		return nil, err
 	}
-
-	jobs, err := extractJobInfo(jobLinks)
-	if err != nil {
-		return nil, err
-	}
+	log.Printf("%v, %v, %v", algoliaApiKey, algoliaAppId, algoliaIndex)
 
 	return jobs, nil
 }
 
-func getJobLinks(ctx context.Context) ([]string, error) {
-	jobLinks := make([]string, 0)
-
-	if err := chromedp.Run(ctx, chromedp.Navigate(baseUrl+"/remote")); err != nil {
-		return nil, err
+func getAlgoliaParams() (string, string, string, error) {
+	var apiKey, appId, index string
+	jsSource := "https://careers.datadoghq.com/assets/scripts/main-YQ6Q5FDQ.js"
+	res, err := http.Get(jsSource)
+	if err != nil {
+		log.Printf("Failed to get js file: %v", err)
+		return apiKey, appId, index, err
+	}
+	body, err := io.ReadAll(res.Body)
+	sb := string(body)
+	if err != nil {
+		return apiKey, appId, index, err
+	}
+	if res.StatusCode != 200 {
+		return apiKey, appId, index, fmt.Errorf("failed to get %v. response: %v", jsSource, sb)
+	}
+	r, err := regexp.Compile(`ALGOLIA_APPLICATION:"([^"]+)"`)
+	if err != nil {
+		return apiKey, appId, index, err
+	}
+	if m := r.FindStringSubmatch(sb); m != nil {
+		appId = m[1]
+	}
+	r, err = regexp.Compile(`ALGOLIA_PUBLIC_TOKEN:"([^"]+)"`)
+	if err != nil {
+		return apiKey, appId, index, err
+	}
+	if m := r.FindStringSubmatch(sb); m != nil {
+		apiKey = m[1]
+	}
+	r, err = regexp.Compile(`ALGOLIA_INDEX:"([^"]+)"`)
+	if err != nil {
+		return apiKey, appId, index, err
+	}
+	if m := r.FindStringSubmatch(sb); m != nil {
+		index = m[1]
 	}
 
-	for {
-		jobLinks = append(jobLinks, getJobLinksFromCurrentPage(ctx)...)
-		log.Println(len(jobLinks))
-		err := chromedp.Run(ctx, chromedp.Click("a.ais-Pagination-link[aria-label=\"Next\"]", chromedp.AtLeast(0)))
-		if err != nil {
-			log.Println(err)
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-
-	return jobLinks, nil
-}
-
-func getJobLinksFromCurrentPage(ctx context.Context) []string {
-	var nodes []*cdp.Node
-	jobLinks := make([]string, 0)
-
-	if err := chromedp.Run(ctx, chromedp.Nodes("button.job-card > a[href]", &nodes, chromedp.ByQueryAll)); err != nil {
-		log.Println(err)
-	} else {
-		for _, node := range nodes {
-			if href, found := node.Attribute("href"); found {
-				jobLinks = append(jobLinks, baseUrl+href)
-			}
-		}
-	}
-	return jobLinks
-}
-
-func extractJobInfo(urls []string) ([]models.Job, error) {
-	jobs := make([]models.Job, 0)
-	c := colly.NewCollector(colly.AllowedDomains(baseDomain))
-
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL)
-	})
-	c.OnScraped(func(r *colly.Response) {
-		jobs[len(jobs)-1].Link = r.Request.URL.String()
-	})
-	c.OnError(func(r *colly.Response, err error) {
-		log.Printf("Failed to visit %v: %v", r.Request.URL, err)
-	})
-	c.OnHTML("main > h2", func(h *colly.HTMLElement) {
-		jobs[len(jobs)-1].Title = h.Text
-	})
-	c.OnHTML("main > h2 + div", func(h *colly.HTMLElement) {
-		jobs[len(jobs)-1].Location = h.Text
-	})
-	c.OnHTML(".job-description", func(h *colly.HTMLElement) {
-		jobs[len(jobs)-1].Description = h.Text
-	})
-	for _, url := range urls {
-		job := models.Job{Company: company}
-		jobs = append(jobs, job)
-		if err := c.Visit(url); err != nil {
-			log.Printf("Error while extracting job info in %v: %v", url, err)
-		}
-	}
-	return jobs, nil
+	return apiKey, appId, index, nil
 }
