cd7345978f26f8e337e83b8bf8d027c9d3499a13
Doryan Dahon <ddahon@gmail.com>
2024-03-16 01:26:21 +0100

    refacto for lumenalta

--- internal/scraper/companies/lumenalta/lumenalta.go
+++ internal/scraper/companies/lumenalta/lumenalta.go
@@ -2,15 +2,12 @@ package lumenalta
 
 import (
 	"errors"
-	"log"
 
 	"github.com/ddahon/jobboard/internal/pkg/models"
-	"github.com/gocolly/colly/v2"
+	"github.com/ddahon/jobboard/internal/scraper"
 )
 
 var companyShortname = "lumenalta"
-var baseDomain = "lumenalta.com"
-var baseUrl = "https://" + baseDomain
 var company *models.Company
 
 func Scrape() ([]models.Job, error) {
@@ -18,47 +15,15 @@ func Scrape() ([]models.Job, error) {
 	if company == nil {
 		return nil, errors.New("Cannot retrieve company for shortname " + companyShortname + ". Aborting scraping for this company.")
 	}
-	jobLinks, err := getJobLinks()
+
+	bs := scraper.BaseScraper{BaseDomain: "lumenalta.com", StartingUrl: "https://lumenalta.com/remote-jobs", Company: company, CompanyShortname: companyShortname}
+	jobLinks, err := scraper.GetJobLinks(scraper.Selector{Type: scraper.XMLSelector, Value: "//a[contains(@href, \"/jobs/\")]"}, bs)
 	if err != nil {
 		return nil, err
 	}
+	locSelector := scraper.Selector{}
+	descSelector := scraper.Selector{Type: scraper.HTMLSelector, Value: "//h4[contains(text(), 'About the Role')]/following::p[1]"}
+	titleSelector := scraper.Selector{Type: scraper.HTMLSelector, Value: "//h1[contains(@class, 'hero-header')]"}
 
-	return extractJobInfo(jobLinks)
-}
-
-func getJobLinks() ([]string, error) {
-	var jobLinks []string
-	c := colly.NewCollector(colly.AllowedDomains(baseDomain))
-	c.OnXML("//a[contains(@href, \"/jobs/\")]", func(x *colly.XMLElement) {
-		jobLinks = append(jobLinks, baseUrl+"/remote-jobs/"+x.Attr("href"))
-	})
-	err := c.Visit(baseUrl + "/remote-jobs")
-	return jobLinks, err
-}
-
-func extractJobInfo(urls []string) ([]models.Job, error) {
-	jobs := make([]models.Job, 0)
-	c := colly.NewCollector(colly.AllowedDomains(baseDomain))
-	c.OnScraped(func(r *colly.Response) {
-		jobs[len(jobs)-1].Link = r.Request.URL.String()
-		jobs[len(jobs)-1].Location = "Remote"
-	})
-	c.OnError(func(r *colly.Response, err error) {
-		log.Printf("Failed to visit %v: %v", r.Request.URL, err)
-	})
-	c.OnXML("//h4[contains(text(), 'About the Role')]/following::p[1]", func(x *colly.XMLElement) {
-		jobs[len(jobs)-1].Description = x.Text
-	})
-	c.OnXML("//h1[contains(@class, 'hero-header')]", func(x *colly.XMLElement) {
-		jobs[len(jobs)-1].Title = x.Text
-	})
-
-	for _, url := range urls {
-		job := models.Job{Company: company}
-		jobs = append(jobs, job)
-		if err := c.Visit(url); err != nil {
-			log.Printf("Error while extracting job info in %v: %v", url, err)
-		}
-	}
-	return jobs, nil
+	return scraper.ExtractJobInfo(jobLinks, locSelector, descSelector, titleSelector, bs)
 }
