2d7e3bd64624212a5e1e458bb3b47a949b1924fb
Doryan Dahon <ddahon@gmail.com>
2024-03-21 21:31:24 +0100

    add tailwind

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </main>
 </template>
